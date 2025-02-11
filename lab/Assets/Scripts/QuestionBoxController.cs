db8e71fa8c820b5cc8d0c99bbdcf36696f568190
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-02-11 15:08:00 +0800

    add disabling for question box

--- lab/Assets/Scripts/QuestionBoxController.cs
+++ lab/Assets/Scripts/QuestionBoxController.cs
@@ -8,29 +8,43 @@ public class QuestionBoxController : MonoBehaviour
     public Animator coinAnimator;
     public AudioSource questionBoxAudio;
     public AudioClip questionBoxSpawnCoin;
+    public bool beenHit = false;
+    public Sprite disabledSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player") && !beenHit)
         {
             // update animator state
             coinAnimator.Play("Coin-Flash");
             questionBoxAudio.PlayOneShot(questionBoxSpawnCoin);
+            beenHit = true;
+
+
         }
 
 
 
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && beenHit)
+        {
+            GetComponent<SpriteRenderer>().sprite = disabledSprite;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
     }
 }
