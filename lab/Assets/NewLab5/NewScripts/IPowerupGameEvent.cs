68cd44faa4f0226d4ac1e7f702981f8c892dfb66
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-01 17:40:01 +0800

    remove ipowerup scripts

--- lab/Assets/NewLab5/NewScripts/IPowerupGameEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "IPowerupGameEvent", menuName = "ScriptableObjects/IPowerupGameEvent", order = 5)]
-public class IPowerupGameEvent : GameEvent<IPowerup>
-{
-
-}
