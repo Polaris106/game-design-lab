afc4f70a004d9d2d406f7b54eaac60adf804c0c8
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2025-03-01 17:20:09 +0800

    create event+listener for 3 types

---
+++ lab/Assets/NewLab5/NewScripts/IPowerupGameEvent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IPowerupGameEvent", menuName = "ScriptableObjects/IPowerupGameEvent", order = 5)]
+public class IPowerupGameEvent : GameEvent<IPowerup>
+{
+
+}
